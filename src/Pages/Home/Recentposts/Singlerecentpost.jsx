bfede58d33b2a6ca78fd045995b663daea927661
Thouhid98 <thouhidhussain98@gmail.com>
2023-11-09 11:41:19 +0600

    Best Blogger section added

--- src/Pages/Home/Recentposts/Singlerecentpost.jsx
+++ src/Pages/Home/Recentposts/Singlerecentpost.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Singlerecentpost = ({recent}) => {
+// import { Viewer } from 'react-photo-view';
+// import { PhotoProvider, PhotoView } from 'react-photo-view';
+
+
+const Singlerecentpost = ({ recent }) => {
     const { _id, title, shortdes, category, date, photo, longdes } = recent;
-   
+
     return (
+
         <div>
-             <div className="card w-96 lg:h-[600px]  bg-gray-100 shadow-xl mb-10">
+            <div className="card w-96 lg:h-[600px]  bg-gray-100 shadow-xl mb-10">
+
                 <figure className="px-10 pt-10">
                     <img src={photo} alt="blogs" className="rounded-xl h-56" />
                 </figure>
+
+                {/* <PhotoView >
+                    <img src={photo} alt={title} className="rounded-xl h-56" />
+                </PhotoView> */}
+
+
                 <div className="card-body items-center text-center">
                     <h2 className="card-title text-3xl text-[#FF3811] font-bold">{title}</h2>
                     <p className='text-black font-medium'>Published : {date}</p>
@@ -25,13 +36,14 @@ const Singlerecentpost = ({recent}) => {
                         {/* Old Link  */}
 
 
-                        {/* Update  */}                     
+                        {/* Update  */}
                         {/* <button onClick={handleWishlist} className='btn btn-neutral'>Add to Wishlist</button> */}
                         {/* </Link> */}
                     </div>
                 </div>
             </div>
         </div>
+        
     );
 };
 
